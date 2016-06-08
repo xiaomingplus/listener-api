9082940d28fd2bcdb062b6efb6dd6e9efe761bca
小明plus <xiaomingplus@qq.com>
2016-06-08 22:29:32 +0800

    for update

--- examples/timeNotice.js
+++ examples/timeNotice.js
@@ -4,48 +4,45 @@
 const axios = require('axios');
 const apiUrl = require('../config').localApiUrl;
 const appAlias = 'time';
-
-const push = (text) =>{
-  return axios(
-      {
-        method:"post",
-        url:apiUrl+"/channels/"+appAlias+"/messages",
-        data:{
-          text:text
-        }
-      }
-    );
+const push = (text) => {
+  return axios({
+    method: "post",
+    url: apiUrl + "/channels/" + appAlias + "/messages",
+    data: {
+      text: text
+    }
+  });
 };
 
-var x = new Date();
-console.log(x);
-console.log('getTime:'+x.getTime());
+const hourReport = () => {        
+  const time = new Date();        
+  const hours = time.getHours();        
+  const mins = time.getMinutes();        
+  const secs = time.getSeconds();        
+  const next = ((60 - mins) * 60 - secs) * 1000;        
+  setTimeout(hourReport, next);        
+  if (mins === 0) {
+    const text = "整点为您报时，现在是北京时间：" + time.toLocaleString(["zh-CN", "en-US"], {
+      timeZone: "Asia/Shanghai"
+    });            
+    console.log(text);
+    push(text).then((r) => {
+      // console.log(r.data);
+    }, (e) => {
+      console.log(e.data);
+    });        
+  } else {
+    const content = '整点报时已启动,现在是北京时间' + time.toLocaleString(["zh-CN", "en-US"], {
+      timeZone: "Asia/Shanghai"
+    });
+    console.log(content);
+    push(content).then((r) => {
+      // console.log(r.data);
+    }, (e) => {
+      console.log(e.data);
+    });
 
-const hourReport = () => {
-        const time = new Date();
-        const hours = time.getHours();
-        const mins = time.getMinutes();
-        const secs = time.getSeconds();
-        const next = ((60 - mins) * 60 - secs) * 1000;
-        setTimeout(hourReport, next);
-        if (mins === 0) {
-            const text = "整点为您报时，现在是北京时间："+time.toLocaleString(["zh-CN","en-US"],{timeZone:"Asia/Shanghai"});
-            console.log(text);
-            push(text).then((r)=>{
-              // console.log(r.data);
-            },(e)=>{
-              console.log(e.data);
-            });
-        }else{
-            const content = '整点报时已启动,现在是北京时间'+time.toLocaleString(["zh-CN","en-US"],{timeZone:"Asia/Shanghai"});
-            console.log(content);
-            push(content).then((r)=>{
-              // console.log(r.data);
-            },(e)=>{
-              console.log(e.data);
-            });
-
-        }
-    }
+  }    
+}
 
 hourReport();
