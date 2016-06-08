b0bd630f4d3cc046fba48df6e54a8a31a6ee7469
小明plus <xiaomingplus@qq.com>
2016-06-08 22:19:54 +0800

    for update

--- examples/timeNotice.js
+++ examples/timeNotice.js
@@ -25,18 +25,18 @@ const hourReport = () => {
         const next = ((60 - mins) * 60 - secs) * 1000;
         setTimeout(hourReport, next);
         if (mins === 0) {
-            const text = "整点为您报时，现在是北京时间："+time.toLocaleString("zh-CN",{timeZone:"Asia/Shanghai"});
+            const text = "整点为您报时，现在是北京时间："+time.toLocaleString(["zh-CN","en-US"],{timeZone:"Asia/Shanghai"});
             console.log(text);
             push(text).then((r)=>{
-              console.log(r.data);
+              // console.log(r.data);
             },(e)=>{
               console.log(e.data);
             });
         }else{
-            const content = '整点报时已启动,现在是北京时间'+time.toLocaleString("zh-CN",{timeZone:"Asia/Shanghai"});
+            const content = '整点报时已启动,现在是北京时间'+time.toLocaleString(["zh-CN","en-US"],{timeZone:"Asia/Shanghai"});
             console.log(content);
             push(content).then((r)=>{
-              console.log(r.data);
+              // console.log(r.data);
             },(e)=>{
               console.log(e.data);
             });
