ad870479202b828276bd2fbe4b2a5ed3c4353cd4
小明plus <xiaomingplus@qq.com>
2016-06-09 13:51:07 +0800

    for update

---
+++ examples/channelsBody.js
@@ -0,0 +1,31 @@
+const time = {
+  "prefix": "报时",
+  "allow_config": 1,
+  "need_verified": 0,
+  "description": "每天的整点为你报时",
+  "avatar": "https://learnenglishkids.britishcouncil.org/sites/kids/files/image/RS2038_Clock%20col-web_1.jpg",
+  "alias": "time",
+  "type": "common",
+  "name": "整点报时"
+}
+
+const water = {
+  "alias": "water",
+  "name": "喝杯水吧",
+  "prefix": "喝水提醒",
+  "description": "每隔一小时就提醒该喝水了。",
+  "allow_config": 1,
+  "need_verified": 0,
+  "avatar": "http://ext.pimg.tw/casanovab110/1409198524-1378234090.jpg"
+}
+
+const test = {
+  "prefix": "测试",
+  "allow_config": 1,
+  "need_verified": 0,
+  "description": "随时都可能收到我的任意消息",
+  "avatar": "http://i3.sinaimg.cn/video/2009/0724/2009724102657.jpg",
+  "alias": "test",
+  "type": "common",
+  "name": "监听者测试主题"
+}
